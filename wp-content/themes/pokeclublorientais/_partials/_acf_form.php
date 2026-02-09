27777ab7f68c3b6a27688f458bc1a44dfad72cb3
Julien Degermann <julien@MacBook-Air-de-Julien.local>
2026-02-09 11:14:27 +0100

    contact page and form

---
+++ wp-content/themes/pokeclublorientais/_partials/_acf_form.php
@@ -0,0 +1,52 @@
+
+<form method="post" id="acf-group_6968a3a2613a1" class="postbox acf-postbox">
+  <h3 class="section-title hndle ui-sortable-handle">Formulaire de contact</h2>
+
+    <div class="inside acf-fields -top">
+      <div class="acf-field acf-field-textarea acf-field-6968a3a2cdb00" data-name="contact_first_name" data-type="textarea" data-key="field_6968a3a2cdb00">
+        <div class="acf-label">
+          <label for="acf-field_6968a3a2cdb00">Prénom</label>
+        </div>
+        <div class="acf-input">
+          <textarea id="acf-field_6968a3a2cdb00" name="acf[field_6968a3a2cdb00]" rows="8"></textarea>
+        </div>
+      </div>
+      <div class="acf-field acf-field-text acf-field-6968a3dfcdb01" data-name="contact_last_name" data-type="text" data-key="field_6968a3dfcdb01">
+        <div class="acf-label">
+          <label for="acf-field_6968a3dfcdb01">Nom</label>
+        </div>
+        <div class="acf-input">
+          <div class="acf-input-wrap"><input type="text" id="acf-field_6968a3dfcdb01" name="acf[field_6968a3dfcdb01]"></div>
+        </div>
+      </div>
+      <div class="acf-field acf-field-email acf-field-6968a3eccdb02" data-name="contact_email" data-type="email" data-key="field_6968a3eccdb02">
+        <div class="acf-label">
+          <label for="acf-field_6968a3eccdb02">E-mail</label>
+        </div>
+        <div class="acf-input">
+          <div class="acf-input-wrap"><input type="email" id="acf-field_6968a3eccdb02" name="acf[field_6968a3eccdb02]"></div>
+        </div>
+      </div>
+      <div class="acf-field acf-field-select acf-field-6968a3facdb03" data-name="contact_object" data-type="select" data-key="field_6968a3facdb03">
+        <div class="acf-label">
+          <label for="acf-field_6968a3facdb03">Objet du message</label>
+        </div>
+        <div class="acf-input">
+          <select id="acf-field_6968a3facdb03" class="" name="acf[field_6968a3facdb03]" data-ui="0" data-ajax="0" data-multiple="0" data-placeholder="Sélectionner" data-allow_null="0">
+            <option value="Devis">Devis</option>
+            <option value="Information">Information</option>
+            <option value="Recrutement">Recrutement</option>
+          </select>
+        </div>
+      </div>
+      <div class="acf-field acf-field-textarea acf-field-6968a47bcdb04" data-name="contact_message" data-type="textarea" data-key="field_6968a47bcdb04">
+        <div class="acf-label">
+          <label for="acf-field_6968a47bcdb04">Message</label>
+        </div>
+        <div class="acf-input">
+          <textarea id="acf-field_6968a47bcdb04" name="acf[field_6968a47bcdb04]" rows="8"></textarea>
+        </div>
+      </div>
+    </div>
+    <input type="submit" value="Envoyer" name="acf_form_submit" class="button button-primary button-large">
+</div>
